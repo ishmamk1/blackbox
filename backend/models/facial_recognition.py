d187f39b065467cf8cb7fe51cefce27c036f64a9
Nakib Abedin <nakibabedin@gmail.com>
2025-02-08 14:51:54 -0500

    add testing code for model

--- backend/models/facial_recognition.py
+++ backend/models/facial_recognition.py
@@ -31,10 +31,31 @@ def intruder_alert( encoded_image ) -> bool:
     faces = face_recognition.face_locations(image)
     facial_encodings_in_frame = face_recognition.face_encodings(image, faces)
 
-    # if the person in frame is anyone we know, then set off an alarm
+    if not facial_encodings_in_frame:
+        return False  
+
+    # Check each detected face against known faces
     for uncertain_face in facial_encodings_in_frame:
-        for known_face in uncertain_face:
-            if( face_recognition.compare_faces([known_face], uncertain_face)[0] ):
-                return False
+        matches = face_recognition.compare_faces(recognized_faces, uncertain_face)
+        
+        if any(matches):
+            return False  
 
     return True
+
+if __name__ == "__main__":
+    with open("../familiar_faces/known_face.png", "rb") as file:
+        known_encoded_image = base64.b64encode(file.read()).decode('utf-8')
+    learn_face(known_encoded_image)
+
+    with open("../familiar_faces/unknown_face.png", "rb") as file:
+        unknown_encoded_image = base64.b64encode(file.read()).decode('utf-8')
+    
+    with open("../familiar_faces/another_known_face.png", "rb") as file:
+        another_known_encoded_image = base64.b64encode(file.read()).decode('utf-8')
+    
+    print( intruder_alert( unknown_encoded_image ) )
+    print( intruder_alert(known_encoded_image) )
+    print( intruder_alert(another_known_encoded_image) )
+    
+    
